e0c479b48e1c660ae61da9bd38dbcafbe1d3ae59
Sergey Dmitriev <51058739+0niel@users.noreply.github.com>
2024-07-25 14:10:07 +0300

    feat: Refactor config.py to split ADMIN_USERNAMES correctly

--- config.py
+++ config.py
@@ -1,20 +1,31 @@
-import os
-from dotenv import load_dotenv
-from pydantic import BaseSettings, validator
+from typing import Any, List, Optional, Tuple, Type
+from pydantic_settings import BaseSettings, EnvSettingsSource, PydanticBaseSettingsSource
+from pydantic import Field, validator
+from pydantic.fields import FieldInfo
 
-load_dotenv()
+
+class MyCustomSource(EnvSettingsSource):
+    def prepare_field_value(self, field_name: str, field: FieldInfo, value: Any, value_is_complex: bool) -> Any:
+        if field_name == "ADMIN_USERNAMES" and value:
+            return value.split(",")
+        return super().prepare_field_value(field_name, field, value, value_is_complex)
 
 
 class Config(BaseSettings):
     PERSPECTIVE_API_KEY: str
     TELEGRAM_BOT_TOKEN: str
-    ADMIN_USERNAMES: list[str]
+    ADMIN_USERNAMES: Optional[List[str]] = Field(default=None)
 
-    @validator("ADMIN_USERNAMES", pre=True, each_item=True)
-    def split_admin_usernames(cls, value):
-        if isinstance(value, str):
-            return value.split(",")
-        return value
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: Type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> Tuple[PydanticBaseSettingsSource, ...]:
+        return (MyCustomSource(settings_cls),)
 
 
 config = Config()
